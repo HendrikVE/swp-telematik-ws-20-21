27dc2738af4e4ad5e258d26bcbf9765fcd694c4c
HendrikVE <hendrik1995@msn.com>
2018-06-30 01:42:30 +0200

    using new method to generate all certs and keys

--- openHABian/setup_openhab_pi.py
+++ openHABian/setup_openhab_pi.py
@@ -2,9 +2,11 @@
 #  -*- coding:utf-8 -*-
 
 import os
+import textwrap
 
 from fabric.api import env, task, run, sudo, execute, put
 from fabric.context_managers import cd
+from fabric.contrib.files import append
 
 from config import config as config
 
@@ -39,7 +41,7 @@ def add_sudo_user():
 
 @task
 def copy_openhab_files():
-    run('echo "copy config files for openhab2"')
+    print('copy config files for openhab2')
 
     res_path = os.path.join('res', 'openhab2')
     dest_path = os.path.join(os.sep, 'etc', 'openhab2')
@@ -74,42 +76,9 @@ def setup_ssl_for_mosquitto():
     certificates generated as shown here: https://jamielinux.com/docs/openssl-certificate-authority/index.html
     """
 
-    def create_key(output_file, bits, encryption=True):
-        sudo('openssl genrsa {ENCRYPTION} \
-             -out {FILE} {BITS}'.format(FILE=output_file, BITS=bits, ENCRYPTION='-aes256' if encryption else ''))
-        sudo('chmod 400 {FILE}'.format(FILE=output_file))
-
-    def create_cert_with_key(key_file, output_file, lifetime):
-        sudo('openssl req \
-             -config openssl.cnf \
-             -new -x509 \
-             -sha256 -extensions v3_ca \
-             -days {LIFETIME} \
-             -key {KEY_FILE} \
-             -out {OUTPUT_FILE}'.format(KEY_FILE=key_file, OUTPUT_FILE=output_file, LIFETIME=lifetime))
-
-    def create_cert_with_req(key_file, csr_output_file, cert_output_file, lifetime, extension, config_file_csr, config_file_cert):
-        sudo('openssl req \
-             -config {CONFIG_FILE_CSR} \
-             -new -sha256 \
-             -key {KEY_FILE} \
-             -out {CSR_OUTPUT_FILE}'.format(KEY_FILE=key_file, CSR_OUTPUT_FILE=csr_output_file, CONFIG_FILE_CSR=config_file_csr))
-
-        sudo('openssl ca \
-             -config {CONFIG_FILE_CERT} \
-             -extensions {EXTENSION} \
-             -days {LIFETIME} \
-             -notext -md sha256 \
-             -in {CSR_OUTPUT_FILE} \
-             -out {CERT_OUTPUT_FILE}'.format(LIFETIME=lifetime, CSR_OUTPUT_FILE=csr_output_file, CERT_OUTPUT_FILE=cert_output_file, CONFIG_FILE_CERT=config_file_cert, EXTENSION=extension))
-
-        sudo('chmod 444 {CERT_OUTPUT_FILE}'.format(CERT_OUTPUT_FILE=cert_output_file))
-
-    def verify_cert(ca_file, cert_file):
-        sudo('openssl verify -CAfile {CA_FILE} {CERT_FILE}'.format(CA_FILE=ca_file, CERT_FILE=cert_file))
-
     print('setup SSL for Mosquitto MQTT broker')
 
+    res_path = os.path.join('res', 'mosquitto')
     home_dir = _get_homedir_openhabian()
 
     with cd(home_dir):
@@ -121,65 +90,32 @@ def setup_ssl_for_mosquitto():
 
         with cd(ca_dir):
 
-            # prepare the main directory
-            sudo('mkdir certs crl newcerts private')
-            sudo('chmod 700 private')
-            sudo('touch index.txt')
-            sudo('echo 1000 > serial')
-            put(os.path.join('res', 'mosquitto_certs', 'root-config.txt'), '%s/openssl.cnf' % ca_dir, use_sudo=True)
+            put(os.path.join(res_path, 'generate-CA.sh'), 'generate-CA.sh', use_sudo=True)
+            sudo('chmod +x generate-CA.sh')
 
-            # prepare the directory for intermediates
-            sudo('mkdir intermediate')
+            # create ca cert and server cert + key
+            sudo('IPLIST="192.168.2.110" HOSTLIST="openHABianPi" ./generate-CA.sh')
 
-            ca_intermediate_dir = os.path.join(ca_dir, 'intermediate')
-            with cd(ca_intermediate_dir):
-                sudo('mkdir certs crl csr newcerts private')
-                sudo('chmod 700 private')
-                sudo('touch index.txt')
-                sudo('echo 1000 > serial')
-                put(os.path.join('res', 'mosquitto_certs', 'intermediate-config.txt'), '%s/openssl.cnf' % ca_intermediate_dir, use_sudo=True)
-
-            # root cert and key
-            create_key('private/ca.key.pem', 4096)
-            create_cert_with_key('private/ca.key.pem', 'certs/ca.cert.pem', 7300)
-
-            """
-            # intermediate cert and key
-            create_key('intermediate/private/intermediate.key.pem', 4096)
-
-            create_cert_with_req('intermediate/private/intermediate.key.pem',
-                                 'intermediate/csr/intermediate.csr.pem',
-                                 'intermediate/certs/intermediate.cert.pem',
-                                 3650,
-                                 'v3_intermediate_ca',
-                                 'intermediate/openssl.cnf', 'openssl.cnf')
-
-            verify_cert('certs/ca.cert.pem', 'intermediate/certs/intermediate.cert.pem')
-
-            sudo('cat intermediate/certs/intermediate.cert.pem certs/ca.cert.pem > intermediate/certs/ca-chain.cert.pem')
-            sudo('chmod 444 intermediate/certs/ca-chain.cert.pem')
-            """
-
-            # server cert and key
-            create_key('intermediate/private/server.key.pem', 2048, False)
-
-            create_cert_with_req('intermediate/private/server.key.pem',
-                                 'intermediate/csr/server.csr.pem',
-                                 'intermediate/certs/server.cert.pem',
-                                 375,
-                                 'server_cert',
-                                 'intermediate/openssl.cnf', 'intermediate/openssl.cnf')
-
-            verify_cert('certs/ca.cert.pem', 'intermediate/certs/server.cert.pem')
+            # create client cert + key
+            sudo('IPLIST="192.168.2.110" HOSTLIST="openHABianPi" ./generate-CA.sh client esp32')
 
             # copy certificates to mosquitto
-            sudo('cp certs/ca.cert.pem /etc/mosquitto/ca_certificates/')
-            sudo('cp intermediate/certs/server.cert.pem intermediate/private/server.key.pem /etc/mosquitto/certs/')
+            sudo('cp ca.crt /etc/mosquitto/ca_certificates/')
+            sudo('cp openHABianPi.crt openHABianPi.key /etc/mosquitto/certs/')
 
             # make references in mosquitto config
-            sudo('echo "cafile /etc/mosquitto/ca_certificates/ca.cert.pem" >> /etc/mosquitto/mosquitto.conf')
-            sudo('echo "certfile /etc/mosquitto/certs/server.cert.pem" >> /etc/mosquitto/mosquitto.conf')
-            sudo('echo "keyfile /etc/mosquitto/certs/server.key.pem" >> /etc/mosquitto/mosquitto.conf')
+            listener_1883_config = 'listener 1883 localhost'
+            listener_8883_config = textwrap.dedent("""
+                listener 8883
+                cafile /etc/mosquitto/ca_certificates/ca.crt
+                certfile /etc/mosquitto/certs/openHABianPi.crt
+                keyfile /etc/mosquitto/certs/openHABianPi.key
+                require_certificate true
+                use_identity_as_username true
+            """)
+
+            append('/etc/mosquitto/mosquitto.conf', listener_1883_config, use_sudo=True)
+            append('/etc/mosquitto/mosquitto.conf', listener_8883_config, use_sudo=True)
 
             sudo('service mosquitto restart')
 
