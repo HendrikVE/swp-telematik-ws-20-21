40961c95fd75f3d7ffe5a76e4fafd6779ccc3b1d
HendrikVE <hendrik1995@msn.com>
2018-07-26 12:02:52 +0200

    restart device if WiFi library is freezed

--- ESP32/window_alert/main/ConnectivityManager.cpp
+++ ESP32/window_alert/main/ConnectivityManager.cpp
@@ -65,7 +65,16 @@ public:
                 Serial.println("Connect to WiFi...");
                 WiFi.begin(CONFIG_ESP_WIFI_SSID, CONFIG_ESP_WIFI_PASSWORD);
             }
+
+            int attempts = 0;
             while (WiFi.status() != WL_CONNECTED) {
+
+                attempts++;
+                if (attempts >= 15) {
+                    // restart device in case WiFi library wont connect
+                    ESP.restart();
+                }
+
                 Serial.print(".");
                 delay(1000);
             }
