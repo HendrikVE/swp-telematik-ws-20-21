9ce40e7d7959176e9f9acdc50f978d912a351926
HendrikVE <hendrik1995@msn.com>
2018-08-15 17:20:11 +0200

    removed header

--- ESP32/window_alert/main/MANIFEST.h
+++ ESP32/window_alert/main/MANIFEST.h
@@ -1,11 +1,7 @@
-//
-// Created by hendrik on 23/07/18.
-//
-
 #ifndef WINDOW_ALERT_MANIFEST_H
 #define WINDOW_ALERT_MANIFEST_H
 
-int APP_VERSION_CODE = 0;
+int APP_VERSION_CODE = 1;
 const char* APP_VERSION_NAME = "1.0.0";
 
 #endif //WINDOW_ALERT_MANIFEST_H
