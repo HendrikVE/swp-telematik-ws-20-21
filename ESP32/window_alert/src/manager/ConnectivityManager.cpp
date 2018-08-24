d36ce242d5cb7f893a8b087fee31d82fdea844c3
HendrikVE <hendrik1995@msn.com>
2018-08-24 17:51:29 +0200

    created header file for connectivity manager

--- ESP32/window_alert/src/manager/ConnectivityManager.cpp
+++ ESP32/window_alert/src/manager/ConnectivityManager.cpp
@@ -6,151 +6,98 @@
 #include "MQTT.h"
 #include "../storage/FlashStorage.h"
 
-class ConnectivityManager {
+#include "ConnectivityManager.h"
 
-public:
+ConnectivityManager::ConnectivityManager() {
+    mWifiMutex = xSemaphoreCreateMutex();
+    mMqttMutex = xSemaphoreCreateMutex();
+}
 
-    ConnectivityManager() {
-        mWifiMutex = xSemaphoreCreateMutex();
-        mMqttMutex = xSemaphoreCreateMutex();
-    }
+void ConnectivityManager::begin() {
+    logger.begin(LOG_LEVEL_VERBOSE, &Serial);
+    logger.setPrefix(printTag);
+    logger.setSuffix(printNewline);
+}
 
-    void begin() {
-        logger.begin(LOG_LEVEL_VERBOSE, &Serial);
-        logger.setPrefix(printTag);
-        logger.setSuffix(printNewline);
-    }
+bool ConnectivityManager::checkWifiConnection() {
 
-    bool checkWifiConnection() {
+    if (xSemaphoreTake(mWifiMutex, (TickType_t) 10 ) == pdTRUE) {
 
-        if (xSemaphoreTake(mWifiMutex, (TickType_t) 10 ) == pdTRUE) {
+        if (WiFi.status() != WL_CONNECTED) {
+            logger.notice("Connect to WiFi...");
 
-            if (WiFi.status() != WL_CONNECTED) {
-                logger.notice("Connect to WiFi...");
+            int attempts = 0;
+            while (WiFi.status() != WL_CONNECTED) {
 
-                int attempts = 0;
-                while (WiFi.status() != WL_CONNECTED) {
-
-                    attempts++;
-                    if (attempts >= 60) {
-                        return false;
-                    }
-
-                    delay(500);
+                attempts++;
+                if (attempts >= 60) {
+                    return false;
                 }
-                logger.notice("Connected");
-            }
 
-            xSemaphoreGive(mWifiMutex);
+                delay(500);
+            }
+            logger.notice("Connected");
         }
 
-        return true;
+        xSemaphoreGive(mWifiMutex);
     }
 
-    bool initWifi() {
-        //WiFi.onEvent(WiFiEvent);
-        WiFi.begin(CONFIG_ESP_WIFI_SSID, CONFIG_ESP_WIFI_PASSWORD);
+    return true;
+}
 
-        return checkWifiConnection();
-    }
+bool ConnectivityManager::initWifi() {
+    //WiFi.onEvent(wifiEvent);
+    WiFi.begin(CONFIG_ESP_WIFI_SSID, CONFIG_ESP_WIFI_PASSWORD);
 
-    void turnOnWifi() {
-        WiFi.mode(WIFI_STA);
-    }
+    return checkWifiConnection();
+}
 
-    void turnOffWifi() {
-        WiFi.disconnect();
-        WiFi.mode(WIFI_OFF);
-    }
+void ConnectivityManager::turnOnWifi() {
+    WiFi.mode(WIFI_STA);
+}
 
-    bool checkMqttConnection() {
+void ConnectivityManager::turnOffWifi() {
+    WiFi.disconnect();
+    WiFi.mode(WIFI_OFF);
+}
 
-        if (xSemaphoreTake(mMqttMutex, (TickType_t) 10 ) == pdTRUE) {
+bool ConnectivityManager::checkMqttConnection() {
 
-            if (!mMqttClient.connected()) {
-                logger.notice("Connect to MQTT broker...");
+    if (xSemaphoreTake(mMqttMutex, (TickType_t) 10 ) == pdTRUE) {
 
-                int attempts = 0;
-                while (!mMqttClient.connect(CONFIG_DEVICE_ID, CONFIG_MQTT_USER, CONFIG_MQTT_PASSWORD)) {
-                    //connect has timeout set by mMqttClient.setOptions()
+        if (!mMqttClient.connected()) {
+            logger.notice("Connect to MQTT broker...");
 
-                    attempts++;
-                    if (attempts >= 60) {
-                        return false;
-                    }
+            int attempts = 0;
+            while (!mMqttClient.connect(CONFIG_DEVICE_ID, CONFIG_MQTT_USER, CONFIG_MQTT_PASSWORD)) {
+                //connect has timeout set by mMqttClient.setOptions()
+
+                attempts++;
+                if (attempts >= 60) {
+                    return false;
                 }
-                logger.notice("Connected");
             }
-
-            xSemaphoreGive(mMqttMutex);
+            logger.notice("Connected");
         }
 
-        return true;
+        xSemaphoreGive(mMqttMutex);
     }
 
-    bool initMqtt() {
+    return true;
+}
 
-        mWifiClientSecure.setCACert((char*) ca_crt_start);
-        mWifiClientSecure.setCertificate((char*) client_crt_start);
-        mWifiClientSecure.setPrivateKey((char*) client_key_start);
+bool ConnectivityManager::initMqtt() {
 
-        mMqttClient.setOptions(10, true, 500);
-        mMqttClient.begin(CONFIG_MQTT_SERVER_IP, CONFIG_MQTT_SERVER_PORT, mWifiClientSecure);
+    mWifiClientSecure.setCACert((char*) ca_crt_start);
+    mWifiClientSecure.setCertificate((char*) client_crt_start);
+    mWifiClientSecure.setPrivateKey((char*) client_key_start);
 
-        return checkMqttConnection();
-    }
+    mMqttClient.setOptions(10, true, 500);
+    mMqttClient.begin(CONFIG_MQTT_SERVER_IP, CONFIG_MQTT_SERVER_PORT, mWifiClientSecure);
 
-    MQTTClient* getMqttClient() {
-        return &mMqttClient;
-    }
+    return checkMqttConnection();
+}
 
-private:
-
-    WiFiClientSecure mWifiClientSecure;
-    MQTTClient mMqttClient;
-
-    Logging logger;
-
-    SemaphoreHandle_t mWifiMutex = NULL;
-    SemaphoreHandle_t mMqttMutex = NULL;
-
-    void static WiFiEvent(WiFiEvent_t event) {
-
-        switch(event) {
-
-            case SYSTEM_EVENT_STA_START:
-                Serial.println("SYSTEM_EVENT_STA_START");
-                break;
-
-            case SYSTEM_EVENT_STA_GOT_IP:
-                Serial.println("SYSTEM_EVENT_STA_GOT_IP");
-                break;
-
-            case SYSTEM_EVENT_AP_STACONNECTED:
-                Serial.println("SYSTEM_EVENT_AP_STACONNECTED");
-                break;
-
-            case SYSTEM_EVENT_AP_STADISCONNECTED:
-                Serial.println("SYSTEM_EVENT_AP_STADISCONNECTED");
-                break;
-
-            case SYSTEM_EVENT_STA_DISCONNECTED:
-                Serial.println("SYSTEM_EVENT_STA_DISCONNECTED");
-                break;
-
-            default:
-                break;
-        }
-    }
-
-    static void printTag(Print* _logOutput) {
-        char c[12];
-        sprintf(c, "%s ", "[ConnectivityManager] ");
-        _logOutput->print(c);
-    }
-
-    static void printNewline(Print* _logOutput) {
-        _logOutput->print("\n");
-    }
-
-};
+MQTTClient* ConnectivityManager::getMqttClient() {
+    return &mMqttClient;
+}
---
+++ ESP32/window_alert/src/manager/ConnectivityManager.h
@@ -0,0 +1,85 @@
+#ifndef CONNECTIVITY_MANAGER_H
+#define CONNECTIVITY_MANAGER_H
+
+#include <stdbool.h>
+
+#include "Arduino.h"
+#include "ArduinoLog.h"
+#include "WiFiClientSecure.h"
+#include "MQTT.h"
+#include "../storage/FlashStorage.h"
+
+class ConnectivityManager {
+
+public:
+
+    ConnectivityManager();
+
+    void begin();
+
+    bool checkWifiConnection();
+
+    bool initWifi();
+
+    void turnOnWifi();
+
+    void turnOffWifi();
+
+    bool checkMqttConnection();
+
+    bool initMqtt();
+
+    MQTTClient* getMqttClient();
+
+private:
+
+    WiFiClientSecure mWifiClientSecure;
+    MQTTClient mMqttClient;
+
+    Logging logger;
+
+    SemaphoreHandle_t mWifiMutex = NULL;
+    SemaphoreHandle_t mMqttMutex = NULL;
+
+    void static wifiEvent(WiFiEvent_t event) {
+
+        switch(event) {
+
+            case SYSTEM_EVENT_STA_START:
+                Serial.println("SYSTEM_EVENT_STA_START");
+                break;
+
+            case SYSTEM_EVENT_STA_GOT_IP:
+                Serial.println("SYSTEM_EVENT_STA_GOT_IP");
+                break;
+
+            case SYSTEM_EVENT_AP_STACONNECTED:
+                Serial.println("SYSTEM_EVENT_AP_STACONNECTED");
+                break;
+
+            case SYSTEM_EVENT_AP_STADISCONNECTED:
+                Serial.println("SYSTEM_EVENT_AP_STADISCONNECTED");
+                break;
+
+            case SYSTEM_EVENT_STA_DISCONNECTED:
+                Serial.println("SYSTEM_EVENT_STA_DISCONNECTED");
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    static void printTag(Print* _logOutput) {
+        char c[12];
+        sprintf(c, "%s ", "[ConnectivityManager] ");
+        _logOutput->print(c);
+    }
+
+    static void printNewline(Print* _logOutput) {
+        _logOutput->print("\n");
+    }
+
+};
+
+#endif /*CONNECTIVITY_MANAGER_H*/
