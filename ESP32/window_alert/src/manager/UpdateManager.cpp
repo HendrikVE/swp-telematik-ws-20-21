7934fe8220a671dc328bb7cbe94be89cdced1ec5
leonPreus <leon.preusker@gmail.com>
2020-12-22 16:25:29 +0100

    fixed OTA Update, ESP now connects with WiFiClientSecure object

--- ESP32/window_alert/src/manager/UpdateManager.cpp
+++ ESP32/window_alert/src/manager/UpdateManager.cpp
@@ -6,6 +6,9 @@
 #include "../storage/FlashStorage.h"
 #include "../MANIFEST.h"
 
+#include "WiFi.h"
+#include "WiFiClientSecure.h"
+
 void UpdateManager::begin(const char* host, const char* filename, const char* user, const char* password, const char* deviceID) {
 
     this->mHost = host;
@@ -17,16 +20,24 @@ void UpdateManager::begin(const char* host, const char* filename, const char* us
 
 void UpdateManager::checkForOTAUpdate() {
 
-    char request[256];
-    sprintf(request, "https://%s/%s/%s/%s", this->mHost, this->mDeviceID, String(APP_VERSION_CODE + 1).c_str(), this->mFilename);
 
+/*  char request[256];
+    //sprintf(request, "https://%s/%s/%s/%s", this->mHost, this->mDeviceID, String(APP_VERSION_CODE + 1).c_str(), this->mFilename);
+    //sprintf(request, "https://%s/%s", this->mHost, this->mFilename);
+    sprintf(request, "/%s", this->mFilename);
+    //sprintf(request, "https://192.168.178.28:4443/window_alert.bin");
+    Log.notice("Request is: %s", request);
+    
+
+    Log.notice("Host is: %s", this->mHost);
     mHttpClient.begin(this->mHost, 4443, request, (char*) ca_crt_start, (char*) client_crt_start, (char*) client_key_start);
     mHttpClient.setAuthorization(this->mUser, this->mPassword);
+    //mHttpClient.setUserAgent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0");
 
     int httpCode = mHttpClient.GET();
 
     if (httpCode != HTTP_CODE_OK) {
-        Log.notice("HTTP GET... failed, error: %d", httpCode);
+        Log.notice("HTTP GET... failed in ota, error: %d", httpCode);
 
         Log.notice("Exiting OTA Update");
 
@@ -34,6 +45,32 @@ void UpdateManager::checkForOTAUpdate() {
 
         return;
     }
+*/
+
+    Log.notice("before creation");
+    WiFiClientSecure client;
+    client.setCACert( (char*) ca_crt_start  );
+    client.setCertificate( (char*) client_crt_start  );
+    client.setPrivateKey( (char*) client_key_start  );
+    char request[256];
+    sprintf(request,"https://%s:4443/%s",this->mHost,this->mFilename);
+    if(!mHttpClient.begin(client,request))
+    {
+        Log.notice("Unable to connect");
+        mHttpClient.end();
+        return;
+    }
+    mHttpClient.setAuthorization(this->mUser, this->mPassword);
+    int httpCode = mHttpClient.GET();
+    if (httpCode != HTTP_CODE_OK) 
+    {
+        Log.notice("HTTP GET... failed in ota, error: %d", httpCode);
+        Log.notice("Exiting OTA Update");
+        mHttpClient.end();
+        return;
+    }
+
+
 
     int contentLength = mHttpClient.getSize();
     Log.notice("Got %d bytes from server", contentLength);
