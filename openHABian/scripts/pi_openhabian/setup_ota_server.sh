5a88b846228cbf252cc10eacbb98ed74ed430eec
HendrikVE <hendrik1995@msn.com>
2021-02-01 11:03:51 +0100

    dont modify copied template

--- openHABian/scripts/pi_openhabian/setup_ota_server.sh
+++ openHABian/scripts/pi_openhabian/setup_ota_server.sh
@@ -7,9 +7,12 @@ source config.sh
 
 # install nginx
 sudo apt install nginx
-#replace SERVER_IP in ota.conf with IP address, the certs are alredy in the resource dir
-sed -i -e "s/SERVER_IP/$HOST_IPV4/g" "$RES_DIR/nginx/conf.d/ota.conf"
-#move config file
+
+# copy config file
 sudo cp "$RES_DIR/nginx/conf.d/ota.conf" /etc/nginx/conf.d
-#restart nginx
+
+# replace SERVER_IP in ota.conf with IP address, the certs are already in the resource dir
+sudo sed -i -e "s/SERVER_IP/$HOST_IPV4/g" /etc/nginx/conf.d/ota.conf
+
+# restart nginx
 sudo service nginx restart
